1710514d246e40139843b2d00c679b41fd819a5c
kalem868 <kalemmalek123@gmail.com>
2023-09-07 17:09:17 -0400

    adjusted

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new Client({
+  host: process.env.RENDER_DATABASE_HOST, // e.g., 'dpg-********-a.oregon-postgres.render.com'
   //@ts-ignore
-  port: process.env.DATABASE_PORT,
-  user: process.env.DATABASE_USERNAME,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
+  port: process.env.RENDER_DATABASE_PORT, // e.g., 5432 (or whatever port your database uses)
+  user: process.env.RENDER_DATABASE_USERNAME, // e.g., 'wte_user'
+  password: process.env.RENDER_DATABASE_PASSWORD, // Your database password
+  database: process.env.RENDER_DATABASE_NAME, // e.g., 'wte'
+  ssl: {
+    rejectUnauthorized: false, // Depending on the provider, you might need this to connect via SSL
+  },
 });
 
 //this is to ensure that numeric values return as numbers and not text
